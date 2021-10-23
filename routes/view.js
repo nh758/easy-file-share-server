464b293c1611a1ae5c56b9b6a6313a511af586d1
wongpratan <wongpratan@hotmail.com>
2021-10-23 11:56:22 +0700

    +Add a service file

--- routes/view.js
+++ routes/view.js
@@ -1,6 +1,6 @@
 // NOTE: require("services/");
 
-module.exports = class viewRoute {
+module.exports = class ViewRoute {
 	static init(app) {
 		app.get('/', (req, res) => {
 			// res.render
---
+++ services/FileService.js
@@ -0,0 +1,4 @@
+module.exports = class FileService {
+	static get() {
+	}
+}
