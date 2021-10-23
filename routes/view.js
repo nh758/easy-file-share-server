bae9d4cf71307072242db4ebafc68e4e0e725e0a
wongpratan <wongpratan@hotmail.com>
2021-10-23 13:26:08 +0700

    +Add response at start route

--- routes/view.js
+++ routes/view.js
@@ -3,7 +3,10 @@
 module.exports = class ViewRoute {
 	static init(app) {
 		app.get('/', (req, res) => {
+			res.json({ ok: "ok" });
 			// res.render
+
+			// call any function of service
 		});
 	}
 }
