466fed1345177ada99ae5b2a38a6392d66df94a9
nate <7259@pm.me>
2021-10-24 10:03:51 +0700

    Fix error in delete

--- services/FileService.js
+++ services/FileService.js
@@ -80,14 +80,8 @@ module.exports = class FileService {
       return res.status(400).send("Unauthorized");
     }
     const folder = user.folderName;
-
     const deleteFile = req.params.filename;
-
     const deletePath = path.join(__dirname, "../uploads", folder, deleteFile);
-
-    // Make the director if it's missing
-    if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
-    const urls = {};
 
     fs.rmSync(deletePath);
 
